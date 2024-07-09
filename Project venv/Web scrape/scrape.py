4232048f48e2f80aa30943484f9005e2b21c749f
ARnord-Trigger <learner9521@gmail.com>
2024-07-09 17:49:37 +0530

    webscraping for BPI done

--- Project venv/Web scrape/scrape.py
+++ Project venv/Web scrape/scrape.py
@@ -5,6 +5,7 @@ import time
 import csv
 import zipfile
 import os
+import pandas as pd
 # import extract
 
 def download_data():
@@ -67,50 +68,59 @@ def download_data():
             time.sleep(sleep_time)
         else:
             pass          
-    folder_path_dic = {'BPI_zip' : 'D:\SummAR_24-25\Electron-Accelerator-Summer2024-25-project-\Project venv\Web scrape\Extracted\Zip_BPI'}
+    
+    def looping_over_time( *list  ):
+        for lst in list:
+            tab_clicker(path= lst[0] , keys= lst[1] , sleep_time= lst[2])
+    # links to all infonet data
+    RRCAT_infonet_dict = {'BPI_data':'http://srs2.cat.ernet.in:8100/servlet/Indus2BPIDataDownloadFormHA',
+                          'Beam current & Beam energy' : 'http://srs2.cat.ernet.in/srsha/srs.htm'  }
+    #essential folder used in scarping
+    folder_path_dict = {'Extracted/Merged_BPI' : 'D:\SummAR_24-25\Electron-Accelerator-Summer2024-25-project-\Project venv\Web scrape\Extracted\Zip_BPI',
+                        'Extracted/Extracted_BPI' : 'D:\\SummAR_24-25\\Electron-Accelerator-Summer2024-25-project-\\Project venv\\Web scrape\\Extracted\\Extracted_BPI',
+                        'DB/BPI' : 'D:\\SummAR_24-25\\Electron-Accelerator-Summer2024-25-project-\\Project venv\\Web scrape\\DB\\BPI',
+                        'DB/BC&BE' : 'D:\\SummAR_24-25\\Electron-Accelerator-Summer2024-25-project-\\Project venv\\Web scrape\\DB\\BC_BE'}
+    
+    # Chromedriver and other important files
+    file_path_dict = {'BPI_data_status_csv'   : 'D:\SummAR_24-25\Electron-Accelerator-Summer2024-25-project-\Project venv\Web scrape\BPI_data_status.csv',
+                      'Chromedriver_exe' : r'D:\SummAR_24-25\Electron-Accelerator-Summer2024-25-project-\Project venv\Web scrape\chromedriver.exe'}
+    
+    #All BPI_xpaths included here
+    BPI_XPATH_dict = {'hour_tab' :'/html/body/center/table/tbody/tr/td/form/center[2]/table/tbody/tr[2]/td[4]/select',
+                      'min_tab' : '/html/body/center/table/tbody/tr/td/form/center[2]/table/tbody/tr[2]/td[5]/select',
+                      'time_duration_tab' : '/html/body/center/table/tbody/tr/td/form/center[1]/table/tbody/tr/td[4]/select',
+                      'sample_rate_tab' : '/html/body/center/table/tbody/tr/td/form/center[1]/table/tbody/tr/td[2]/select',
+                      'date_tab' : '/html/body/center/table/tbody/tr/td/form/center[2]/table/tbody/tr[2]/td[1]/select',
+                      'month_tab' : '/html/body/center/table/tbody/tr/td/form/center[2]/table/tbody/tr[2]/td[2]/select',
+                      'year_tab' : '/html/body/center/table/tbody/tr/td/form/center[2]/table/tbody/tr[2]/td[3]/select',
+                      'refresh_tab' : '/html/body/center/table/tbody/tr/td/form/center[3]/input[2]',
+                      'download_tab' : '/html/body/center/table/tbody/tr/td/form/center[3]/input[1]'}
     
     options = webdriver.ChromeOptions()
-    file = "D:\SummAR_24-25\Electron-Accelerator-Summer2024-25-project-\Project venv\Web scrape\BPI.csv"
-    preferences = {"download.default_directory": "D:\\SummAR_24-25\\Electron-Accelerator-Summer2024-25-project-\\Project venv\\Web scrape\\DB\\BPI"}
+    file = file_path_dict['BPI_data_status_csv']
+    preferences = {"download.default_directory": folder_path_dict['DB/BPI']}
     options.add_experimental_option("prefs", preferences)
-    driver = webdriver.Chrome(executable_path=r'D:\SummAR_24-25\Electron-Accelerator-Summer2024-25-project-\Project venv\Web scrape\chromedriver.exe', chrome_options=options)
-    driver.get('http://srs2.cat.ernet.in:8100/servlet/Indus2BPIDataDownloadFormHA')
-    
-    #All xpaths included here
-    XPATH_dict = {'hour_tab' :'/html/body/center/table/tbody/tr/td/form/center[2]/table/tbody/tr[2]/td[4]/select',
-                 'min_tab' : '/html/body/center/table/tbody/tr/td/form/center[2]/table/tbody/tr[2]/td[5]/select',
-                 'time_duration_tab' : '/html/body/center/table/tbody/tr/td/form/center[1]/table/tbody/tr/td[4]/select',
-                 'sample_rate_tab' : '/html/body/center/table/tbody/tr/td/form/center[1]/table/tbody/tr/td[2]/select',
-                 'date_tab' : '/html/body/center/table/tbody/tr/td/form/center[2]/table/tbody/tr[2]/td[1]/select',
-                 'month_tab' : '/html/body/center/table/tbody/tr/td/form/center[2]/table/tbody/tr[2]/td[2]/select',
-                 'year_tab' : '/html/body/center/table/tbody/tr/td/form/center[2]/table/tbody/tr[2]/td[3]/select',
-                 'refresh_tab' : '/html/body/center/table/tbody/tr/td/form/center[3]/input[2]',
-                 'download_tab' : '/html/body/center/table/tbody/tr/td/form/center[3]/input[1]'}
-    
-    
-    
-    #click hour_tab
-    tab_clicker(path = XPATH_dict['hour_tab'] , keys ='0' , sleep_time = 1)
-    #click minute_tab
-    tab_clicker(path = XPATH_dict['min_tab'] , keys ='0' , sleep_time = 1)
-    #click time_duration
-    tab_clicker(path = XPATH_dict['time_duration_tab'] , keys ='4 hrs' , sleep_time = 1)    
+    driver = webdriver.Chrome(executable_path=file_path_dict['Chromedriver_exe'], chrome_options=options)
+    driver.get(RRCAT_infonet_dict['BPI_data'])
+       
     
     #setting current day and prev month 
-    #prev month is chosen because we need to terate from ith day of prv month to ith day of current month 
+    #prev month is chosen because we need to iterate from ith day of prv month to ith day of current month 
     current_time = datetime.datetime.now()
     prev_month_key = current_time.month - 1
     present_day =  current_time.day
     
     # from numeric to half month name using fxn above mmm
     prev_month_value = mmm(prev_month_key) 
-    
+    present_month_value =mmm(current_time.month)
     # selecting month
-    tab_clicker(path = XPATH_dict['month_tab'] , keys = prev_month_value , sleep_time = 1)
+    # tab_clicker(path = BPI_XPATH_dict['month_tab'] , keys = prev_month_value , sleep_time = 1)
+    
+    hour_batch={0,4,8,12,16,20}
     
     #t_day is temp day
+    #this part runs from present day of prev month to last day of prev month 
     t_day = present_day
-    hour_batch={0,4,8,12,16,20}
     maxdays = ddd(prev_month_value)
     while t_day <= maxdays:
         if t_day < 10 :
@@ -118,5 +128,120 @@ def download_data():
         else:
             day = str(t_day)
         date = day + prev_month_value + '2024'
-        if os.path.isfile()        
+        
+        if os.path.isfile(folder_path_dict['Extracted/Merged_BPI'] + 'BPI' + date + '.csv'):
+            t_day += 1
+            continue
+        if t_day == 22:
+            t_day += 1
+            day = '222'
+            
+        for h in hour_batch:
+            try:
+                if h < 10:
+                    hr = padding(h)
+                else:
+                    hr = str(h)
+                looping_over_time([BPI_XPATH_dict['hour_tab'] , hr , 0],
+                                  [BPI_XPATH_dict['min_tab'] ,'0', 1 ],
+                                  [BPI_XPATH_dict['date_tab'] , day , 2 ],
+                                  [BPI_XPATH_dict['month_tab'] , prev_month_value , 0],
+                                  [BPI_XPATH_dict['time_duration_tab'] , '4 hrs' , 0],
+                                  [BPI_XPATH_dict['download_tab'] , None , 0],
+                                  [BPI_XPATH_dict['refresh_tab'] , None , 0])
+                
+                csvData = [{'DATE': day + prev_month_value + '2023', 'TIME': hr + '00 hrs', 'FOUND': 'YES'}]
+                with open(file, 'a') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=fields)
+                    writer.writerows(csvData)
+            except:
+                tab_clicker(path='/html/body/center[2]/a/h2/b')
+                csvData = [{'DATE': day + prev_month_value + '2023', 'TIME': hr + '00 hrs', 'FOUND': 'NO'}]
+                with open(file, 'a') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=fields)
+                    writer.writerows(csvData)
+        
+        # Define the paths
+        zip_folder = folder_path_dict['DB/BPI']
+        extract_folder = folder_path_dict['Extracted/Extracted_BPI']
+        output_folder = folder_path_dict['Extracted/Merged_BPI']
+        merged_file = folder_path_dict['Extracted/Merged_BPI'] + 'BPI' + date + '.csv'
+        # creating csv at location merged_file
+        empty_df = pd.DataFrame()
+        empty_df.to_csv(os.path.join(output_folder, merged_file), index=False)
+        # Extract CSV files from the ZIP files
+        for zip_file in os.listdir(zip_folder):
+            if zip_file.endswith('.zip'):
+                with zipfile.ZipFile(os.path.join(zip_folder, zip_file), 'r') as zip_ref:
+                    zip_ref.extractall(extract_folder)
+        # List all extracted CSV files
+        csv_files = [os.path.join(extract_folder, f) for f in os.listdir(extract_folder) if f.endswith('.csv')]
+        # Merge the CSV files along columns
+        merged_df = pd.concat([pd.read_csv(f) for f in csv_files], axis=1)
+        # Save the merged dataframe to the output folder
+        merged_df.to_csv(os.path.join(output_folder, merged_file), index=False)
+        print(f"Merged file saved to {os.path.join(output_folder, merged_file)}")
+        t_day +=1
     
+    t_day = 1
+    maxdays = ddd(present_month_value)
+    while t_day <= present_day:
+        if t_day < 10 :
+            day = padding(t_day)
+        else:
+            day = str(t_day)
+        date = day + present_month_value + '2024'
+        
+        if os.path.isfile(folder_path_dict['Extracted/Merged_BPI'] + 'BPI' + date + '.csv'):
+            t_day += 1
+            continue
+        if t_day == 22:
+            t_day += 1
+            day = '222'
+            
+        for h in hour_batch:
+            try:
+                if h < 10:
+                    hr = padding(h)
+                else:
+                    hr = str(h)
+                looping_over_time([BPI_XPATH_dict['hour_tab'] , hr , 0],
+                                  [BPI_XPATH_dict['min_tab'] ,'0', ]
+                                  [BPI_XPATH_dict['date_tab'] , day , 2 ],
+                                  [BPI_XPATH_dict['month_tab'] , present_month_value , 0],
+                                  [BPI_XPATH_dict['time_duration_tab'] , '4 hrs' , 0],
+                                  [BPI_XPATH_dict['download_tab'] , None , 0],
+                                  [BPI_XPATH_dict['refresh_tab'] , None , 0])
+                
+                csvData = [{'DATE': day + present_month_value + '2023', 'TIME': hr + '00 hrs', 'FOUND': 'YES'}]
+                with open(file, 'a') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=fields)
+                    writer.writerows(csvData)
+            except:
+                tab_clicker(path='/html/body/center[2]/a/h2/b')
+                csvData = [{'DATE': day + present_month_value + '2023', 'TIME': hr + '00 hrs', 'FOUND': 'NO'}]
+                with open(file, 'a') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=fields)
+                    writer.writerows(csvData)
+        
+        # creating csv at location merged_file
+        empty_df = pd.DataFrame()
+        empty_df.to_csv(os.path.join(output_folder, merged_file), index=False)
+        # Extract CSV files from the ZIP files
+        for zip_file in os.listdir(zip_folder):
+            if zip_file.endswith('.zip'):
+                with zipfile.ZipFile(os.path.join(zip_folder, zip_file), 'r') as zip_ref:
+                    zip_ref.extractall(extract_folder)
+        # List all extracted CSV files
+        csv_files = [os.path.join(extract_folder, f) for f in os.listdir(extract_folder) if f.endswith('.csv')]
+        # Merge the CSV files along columns
+        merged_df = pd.concat([pd.read_csv(f) for f in csv_files], axis=1)
+        # Save the merged dataframe to the output folder
+        merged_df.to_csv(os.path.join(output_folder, merged_file), index=False)
+        print(f"Merged file saved to {os.path.join(output_folder, merged_file)}")    
+        t_day +=1            
+ 
+        
+    
+download_data()
+        
